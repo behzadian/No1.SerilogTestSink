5c2b9d2dc4bf1cbb663117e1fca4250b4408fe0b
Hank <eqpc@hbn.dev>
2026-06-02 14:35:23 +0200

    thread safty added

--- lib/TestSerilogSink.cs
+++ lib/TestSerilogSink.cs
@@ -1,6 +1,7 @@
 ﻿using Serilog.Core;
 using Serilog.Events;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,9 +9,9 @@ namespace No1.SerilogTestSink;
 
 public class TestSerilogSink : ILogEventSink
 {
-	public static IList<LogEvent> LogEvents { get; } = [];
+	public static ConcurrentQueue<LogEvent> LogEvents { get; } = [];
 
 	public void Emit(LogEvent logEvent) {
-		LogEvents.Add(logEvent);
+		LogEvents.Enqueue(logEvent);
 	}
 }
