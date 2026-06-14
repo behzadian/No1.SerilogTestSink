c8f7d91c05f00ea83397c41cd6866c2d37898225
Hank <eqpc@hbn.dev>
2026-06-14 12:49:37 +0200

    #2

--- lib/LogEventExtensions.cs
+++ lib/LogEventExtensions.cs
@@ -6,7 +6,8 @@ namespace No1.SerilogTestSink;
 
 public static class LogEventExtensions
 {
-	const BindingFlags MethodBindingFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static | BindingFlags.NonPublic;
+	[System.Diagnostics.CodeAnalysis.SuppressMessage("Major Code Smell", "S3011:Reflection should not be used to increase accessibility of classes, methods, or fields", Justification = "Safe")]
+	private const BindingFlags MethodBindingFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static | BindingFlags.NonPublic;
 
 	public static bool Match<T>(this LogEvent logEvent, string methodName) {
 		ArgumentNullException.ThrowIfNull(logEvent);
