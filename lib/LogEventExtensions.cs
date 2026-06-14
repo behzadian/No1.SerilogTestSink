0f5504117b09e5d009eb261929be23437d559a63
Hank <eqpc@hbn.dev>
2026-06-14 12:47:05 +0200

    #1

--- lib/LogEventExtensions.cs
+++ lib/LogEventExtensions.cs
@@ -1,9 +1,6 @@
-﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging;
 using Serilog.Events;
-using System;
-using System.Collections.Generic;
 using System.Reflection;
-using System.Text;
 
 namespace No1.SerilogTestSink;
 
